eb3d97df243f6e0fec76eeef0c629cec762b85a9
Ian.Xu <ian@fisheep.cc>
2021-02-04 23:05:15 +0800

    py: add the method that how get cmdline args

---
+++ py/x_cmdline_args.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import sys
+
+if __name__ == '__main__':
+    # python3 x_cmdline_args.py  a1 a2 a3 a4 a5
+    argv = sys.argv
+    argc = len(argv)
+
+    for arg in argv:
+        print(arg)
+    # x_cmdline_args.py
+    # a1
+    # a2
+    # a3
+    # a4
+    # a5
+
+    i = 0
+    while i < argc:
+        print(str(i)+'/'+str(argc) + '  ' + argv[i])
+        i += 1
+    # 0/6  x_cmdline_args.py
+    # 1/6  a1
+    # 2/6  a2
+    # 3/6  a3
+    # 4/6  a4
+    # 5/6  a5
+
