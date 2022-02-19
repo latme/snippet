75ddb62d68eddf7683692623d352d39ac86be49a
Ian.Xu <ian@fisheep.cc>
2022-02-19 08:11:00 +0000

    mk: add get git commit id code

---
+++ mk/git.mk
@@ -0,0 +1,13 @@
+
+GIT_COMMIT   := $(shell git log --pretty=%h -1)
+GIT_MODIFIED := $(shell [ -n "$$(git status -s)" ] && echo "-modified")
+GIT_SRC_VER  := $(GIT_COMMIT)$(GIT_MODIFIED)
+ifeq ($(GIT_SRC_VER),)
+    GIT_SRC_VER = "unkown"
+endif
+export GIT_SRC_VER
+
+
+CFLAGS += -D GIT_SRC_VER="$(GIT_SRC_VER)"
+
+
