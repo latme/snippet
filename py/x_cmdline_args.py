f41b6a0fad3eaa91a4cc3d925141f93da566cd03
Ian.Xu <ian@fisheep.cc>
2024-12-02 10:24:56 +0800

    py: add current file directory to import search path

--- py/x_cmdline_args.py
+++ py/x_cmdline_args.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
+import os
 import sys
+
+# 文件所在目录添加到import搜索路径
+TOPDIR = os.path.dirname(__file__)
+sys.path.append(TOPDIR)
+
 
 if __name__ == '__main__':
     # python3 x_cmdline_args.py  a1 a2 a3 a4 a5
